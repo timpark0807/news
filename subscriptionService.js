cf4a6a3b158aceabdf7fea825603289bf1ca5551
Tim Park <timpark0807@gmail.com>
2020-06-09 19:39:26 -0700

    Push failed actions to SQS queue, cleanup helper methods and improve readability

--- subscriptionService.js
+++ subscriptionService.js
@@ -1,18 +1,20 @@
 const client = require("./twitterClient.js")
 const AWS = require("aws-sdk");
 const dynamodb = new AWS.DynamoDB({region:'us-east-1'});
-
+const sqs = new AWS.SQS({region:'us-east-1'});
 
 // Main Function Handler
 exports.handler = async (events) => {
-    const params = {q: '@stocknewsbot'};
-    const tweets = await promiseGetRawTweets(params);
+    const rawTweets = await promiseGetRawTweets({ q: '@stocknewsbot' });
+    const tweets = await getTweets(rawTweets);
 
     // Iterate backwards through the tweets to process oldest tweet first
     for (let i=tweets.length-1; i>=0; i--){
         const tweet = tweets[i];
         const ticker = tweet.ticker.toUpperCase();
         const params = getQueryParams(tweet);
+
+        // ToDo: Check DynamoDB if tweet has already been processed  
 
         let userData = await promiseGetUserData(params);
 
@@ -27,8 +29,7 @@ exports.handler = async (events) => {
 // Subscribe Functionality 
 async function processSubscribe(data, ticker) {
 
-    const currSubscriptions = data.Item.subscriptions.SS;   
-    const username = data.Item.username.S; 
+    const [currSubscriptions, username] = parseData(data);
 
     // Check that we already not already subscribed to the ticker 
     if (!currSubscriptions.includes(ticker)){
@@ -52,15 +53,14 @@ async function processSubscribe(data, ticker) {
 // Unsubscribe Functionality
 async function processUnSubscribe(data, ticker) {
 
-    const currSubscriptions = data.Item.subscriptions.SS;
-    const username = data.Item.username.S; 
+    const [currSubscriptions, username] = parseData(data);
 
     // Check the ticker we want to unsubscribe from is in the user's current subscriptions 
     if (currSubscriptions.includes(ticker)) {
 
         // Remove that ticker
-        const index = currSubscriptions.indexOf(ticker);
-        currSubscriptions.splice(index, 1);
+        const tickerIndex = currSubscriptions.indexOf(ticker);
+        currSubscriptions.splice(tickerIndex, 1);
 
         // Create an updated item to PUT
         var newParams = {
@@ -81,12 +81,11 @@ async function processUnSubscribe(data, ticker) {
 // Promise Wrappers for Async Functions
 function promiseGetRawTweets(params){
     return new Promise((resolve, reject) => {
-        client.get('search/tweets', params, (error, rawTweets, response) => {
+        client.get('search/tweets', params, (error, data, response) => {
             if (error) {
                 reject(error);
             } else {
-                const tweets = getTweets(rawTweets);
-                resolve(tweets);
+                resolve(data);
             }
         });
     });
@@ -116,18 +115,28 @@ function promisePutItem(params){
     })
 }
 
+function promiseLoadQueue(params) {
+    return new Promise((resolve, reject) => {
+        sqs.sendMessage(params, (error, data) => {
+            if (error) {
+                reject(error);
+            }
+            if (!error){
+                resolve(data);
+            }
+        });         
+    });
+}
 
 // Miscellaneous Helper Functions 
-function getTweets(tweets) {
+async function getTweets(tweets) {
     const processedTweets = [];
     for (let i=0; i<tweets.statuses.length; i++) {
         let currTweet = tweets.statuses[i];
-        let output = getActionAndTicker(currTweet.text);
-        let action = output[0];
-        let ticker = output[1]; 
+        let [action, ticker] = getActionAndTicker(currTweet.text);
+
         if (action !== "subscribe" && action !== "unsubscribe") { 
-            // TODO: Place in SQS to reply to tweet. 
-            continue
+            await promiseLoadQueue({QueueUrl: process.env['SQS_FAIL_URL'], MessageBody: currTweet.text});
         } else {
         let processedTweet = {"user": "@" + currTweet.user.name, 
                                 "message": currTweet.text, 
@@ -147,8 +156,18 @@ function getActionAndTicker(message) {
 
 function getQueryParams(tweet) {
     var params = {
-        Key: {"username": {S:tweet.user}},
+        Key: {
+            "username": {
+                S:tweet.user
+            }
+        },
         TableName: "stocknews-twitter-db"
     }
     return params; 
+}
+
+function parseData(data) {
+    const currSubscriptions = data.Item.subscriptions.SS;
+    const username = data.Item.username.S; 
+    return [currSubscriptions, username];
 }
